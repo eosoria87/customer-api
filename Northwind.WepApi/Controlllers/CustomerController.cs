bea710b59e3fdabe9f6e831eb446ec1c7c661f9d
eosoria87 <eosoria87@gmail.com>
2020-08-28 21:55:18 -0500

    Method in Controller

--- Northwind.WepApi/Controlllers/CustomerController.cs
+++ Northwind.WepApi/Controlllers/CustomerController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Northwind.Models.Models;
 using Northwind.UnitOfWork;
 
 namespace Northwind.WepApi.Controlllers
@@ -19,10 +20,47 @@ namespace Northwind.WepApi.Controlllers
         }
 
         [HttpGet]
-        [Route("int:id")]
+        [Route("id:int")]
         public IActionResult GetById(int id)
         {
             return Ok(_unitOfWork.Customer.GetById(id));
+        }
+
+
+        [HttpGet]
+        [Route("GetPaginatedCustomer/{page:int}/rows:int")]
+        public IActionResult GetPaginated(int page, int rows)
+        {
+            return Ok(_unitOfWork.Customer.CustomerPagedList(page, rows));
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Customer model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            return Ok(_unitOfWork.Customer.Insert(model));
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Customer model)
+        {
+            if (!ModelState.IsValid && _unitOfWork.Customer.Update(model))
+            {
+                return Ok(new { Message = "The Customer is update"});
+            }
+            return BadRequest();
+        }
+
+        [HttpDelete]
+        [Route("id:int")]
+        public IActionResult Delete([FromBody] Customer model)
+        {
+            if (model.Id > 0)
+                return Ok(_unitOfWork.Customer.Delete(model));
+
+            return BadRequest();
         }
     }
 }
